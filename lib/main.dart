d3201f4fbd2a5cd644862657ed6d1a7353976d5c
AyoubTitoun <ia_titoun@esi.dz>
2024-06-28 12:56:49 +0200

    final product without perfection

--- lib/main.dart
+++ lib/main.dart
@@ -36,6 +36,38 @@ class TitleRow extends StatelessWidget {
 
 
 
+
+
+
+
+
+
+class IconCombined extends StatelessWidget {
+
+const IconCombined ({ super.key,
+required this.title, required this.icontitle
+}
+);
+final String title;
+final Icon icontitle;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return(Column(children: [
+      Padding(
+        padding: const EdgeInsets.only(bottom: 4.0),
+        child: icontitle,
+      ), Text(title, style: TextStyle(color: Colors.purple ),),
+
+    ],)
+    );
+  }
+
+}
+
+
+
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
@@ -45,7 +77,20 @@ void main() {
           children: [
             Image.asset('../images/pic2001.jpg'),
             TitleRow(title: 'Villiers-sur-marne lake from here',location: 'Just next to my house in Villiers'),
-          ],
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+            
+              
+              children: [IconCombined(title:'CALL', icontitle: Icon(Icons.call,color: Colors.purple,)),
+            IconCombined(title:'ROUTE', icontitle: Icon(Icons.near_me,color: Colors.purple,)),
+            IconCombined(title:'SHARE', icontitle: Icon(Icons.share,color: Colors.purple,))],
+            ),
+            SizedBox(height: 15,),
+           Container(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Text('Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures  and pine forest, leads you to the lake, which warms to 20  degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.'))
+ ],
+
         
         ),
       )),
