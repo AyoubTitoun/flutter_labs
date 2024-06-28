a3300f92a2a9b2af2f0a2ee489edff4aff73ea5a
AyoubTitoun <ia_titoun@esi.dz>
2024-06-28 14:48:17 +0200

    Final perfection

--- lib/main.dart
+++ lib/main.dart
@@ -66,33 +66,66 @@ final Icon icontitle;
 
 }
 
+class RowButtons extends StatelessWidget {
 
+  const RowButtons ({
+    super.key
+  });
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Padding(
+      padding: const EdgeInsets.all(35.0),
+      child: ( Row( 
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+               IconCombined(title:'CALL', icontitle: Icon(Icons.call,color: Colors.purple,)),
+              IconCombined(title:'ROUTE', icontitle: Icon(Icons.near_me,color: Colors.purple,)),
+              IconCombined(title:'SHARE', icontitle: Icon(Icons.share,color: Colors.purple,))
+      ]
+      
+              
+      
+      )),
+    );
+   
+  }
+
+
+}
+
+
+class FinalProduct extends StatelessWidget {
+
+  const FinalProduct ({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return ( Column(children: [
+       Image.asset('../images/pic2001.jpg'),
+            TitleRow(title: 'Villiers-sur-marne lake from here',location: 'Just next to my house in Villiers'),
+           RowButtons(),
+            SizedBox(height: 15,),
+           Container(
+            padding: EdgeInsets.only(left: 20, right: 20),
+            child: Text('Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures  and pine forest, leads you to the lake, which warms to 20  degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.'))
+    ],)
+      
+      
+      
+      );
+  }}
+
+ 
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(title: Center(child: Text("First Flutter Layout")),),
       body:  SingleChildScrollView(child: Center(
-        child: Column(
-          children: [
-            Image.asset('../images/pic2001.jpg'),
-            TitleRow(title: 'Villiers-sur-marne lake from here',location: 'Just next to my house in Villiers'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-            
-              
-              children: [IconCombined(title:'CALL', icontitle: Icon(Icons.call,color: Colors.purple,)),
-            IconCombined(title:'ROUTE', icontitle: Icon(Icons.near_me,color: Colors.purple,)),
-            IconCombined(title:'SHARE', icontitle: Icon(Icons.share,color: Colors.purple,))],
-            ),
-            SizedBox(height: 15,),
-           Container(
-            padding: EdgeInsets.only(left: 20, right: 20),
-            child: Text('Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures  and pine forest, leads you to the lake, which warms to 20  degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.'))
- ],
-
-        
-        ),
+        child: FinalProduct(),
       )),
        
     ),
