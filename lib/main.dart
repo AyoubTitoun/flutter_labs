5de4aea5349ddce8a2ac47875e2fcce2779b99c0
AyoubTitoun <ia_titoun@esi.dz>
2024-07-02 09:46:23 +0200

    mix of lists use

--- lib/main.dart
+++ lib/main.dart
@@ -15,22 +15,76 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text(title),
         ),
-        body: ListView(
-          children: const <Widget>[
-            ListTile(
-              leading: Icon(Icons.map),
-              title: Text('Map'),
-            ),
-            ListTile(
-              leading: Icon(Icons.photo_album),
-              title: Text('Album'),
-            ),
-            ListTile(
-              leading: Icon(Icons.phone),
-              title: Text('Phone'),
-            ),
-          ],
+        body:
+           Column(
+             children: [
+               Expanded(
+                 child: ListView(
+                    children: const <Widget>[
+                      ListTile(
+                        leading: Icon(Icons.map),
+                        title: Text('Map'),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.photo_album),
+                        title: Text('Album'),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.phone),
+                        title: Text('Phone'),
+                      ),
+                    ],
+                  ),
+               ),
+
+               Expanded(child:ListView(
+  // This next line does the trick.
+  scrollDirection: Axis.horizontal,
+  children: <Widget>[
+    Container(
+      width: 160,
+      color: Colors.red,
+    ),
+    Container(
+      width: 160,
+      color: Colors.blue,
+    ),
+    Container(
+      width: 160,
+      color: Colors.green,
+    ),
+    Container(
+      width: 160,
+      color: Colors.yellow,
+    ),
+    Container(
+      width: 160,
+      color: Colors.orange,
+    ),
+  ],
+),
+),
+Expanded(
+  child: GridView.count(
+    // Create a grid with 2 columns. If you change the scrollDirection to
+    // horizontal, this produces 2 rows.
+    crossAxisCount: 2,
+    // Generate 100 widgets that display their index in the List.
+    children: List.generate(100, (index) {
+      return Center(
+        child: Text(
+          'Item $index',
+          style: Theme.of(context).textTheme.headlineSmall,
         ),
+      );
+    }),
+  ),
+),
+             ],
+           ),
+         
+            
+        
       ),
     );
   }
