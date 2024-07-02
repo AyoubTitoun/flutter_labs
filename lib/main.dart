8c1332b15eb29ca1ecba891314b71a51b5e8795e
AyoubTitoun <ia_titoun@esi.dz>
2024-07-02 09:31:54 +0200

    deal with lists

--- lib/main.dart
+++ lib/main.dart
@@ -1,134 +1,37 @@
 import 'package:flutter/material.dart';
 
+void main() => runApp(const MyApp());
 
-class TitleRow extends StatelessWidget {
-
-  const TitleRow ({super.key, required this.title, required this.location});
-  final String title;
-  final String location;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(32.0),
-      child: (
-        Row(mainAxisAlignment: MainAxisAlignment.center,
-          children: [Expanded(
-            child: Column(
-               crossAxisAlignment: CrossAxisAlignment.start,
-              children: [Padding(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: Text(title,style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-              ), 
-                     Text(location, style :TextStyle(fontWeight: FontWeight.w100,  color: Colors.grey[500])),
-                  ],),
-          ),
-      Icon(Icons.star,  color: Colors.red[500],),
-      Text('41'),
-      
-      ],)
+    const title = 'Basic List';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
+        body: ListView(
+          children: const <Widget>[
+            ListTile(
+              leading: Icon(Icons.map),
+              title: Text('Map'),
+            ),
+            ListTile(
+              leading: Icon(Icons.photo_album),
+              title: Text('Album'),
+            ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text('Phone'),
+            ),
+          ],
+        ),
       ),
     );
   }
-
-}
-
-
-
-
-
-
-
-
-
-class IconCombined extends StatelessWidget {
-
-const IconCombined ({ super.key,
-required this.title, required this.icontitle
-}
-);
-final String title;
-final Icon icontitle;
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return(Column(children: [
-      Padding(
-        padding: const EdgeInsets.only(bottom: 4.0),
-        child: icontitle,
-      ), Text(title, style: TextStyle(color: Colors.purple ),),
-
-    ],)
-    );
-  }
-
-}
-
-class RowButtons extends StatelessWidget {
-
-  const RowButtons ({
-    super.key
-  });
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Padding(
-      padding: const EdgeInsets.all(35.0),
-      child: ( Row( 
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-               IconCombined(title:'CALL', icontitle: Icon(Icons.call,color: Colors.purple,)),
-              IconCombined(title:'ROUTE', icontitle: Icon(Icons.near_me,color: Colors.purple,)),
-              IconCombined(title:'SHARE', icontitle: Icon(Icons.share,color: Colors.purple,))
-      ]
-      
-              
-      
-      )),
-    );
-   
-  }
-
-
-}
-
-
-class FinalProduct extends StatelessWidget {
-
-  const FinalProduct ({super.key});
-
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return ( Column(children: [
-       Image.asset('../images/pic2001.jpg'),
-            TitleRow(title: 'Villiers-sur-marne lake from here',location: 'Just next to my house in Villiers'),
-           RowButtons(),
-            SizedBox(height: 15,),
-           Container(
-            padding: EdgeInsets.only(left: 20, right: 20),
-            child: Text('Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures  and pine forest, leads you to the lake, which warms to 20  degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.'))
-    ],)
-      
-      
-      
-      );
-  }}
-
- 
-
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(title: Center(child: Text("First Flutter Layout")),),
-      body:  SingleChildScrollView(child: Center(
-        child: FinalProduct(),
-      )),
-       
-    ),
-  )
-  );
 }
