e51c9785ff9af8126b36eab9027f5614020f50d0
AyoubTitoun <ia_titoun@esi.dz>
2024-07-03 12:05:38 +0200

    adding text controlleurs

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
+final myController = TextEditingController();
 
 class TitleRow extends StatelessWidget {
-
-  const TitleRow ({super.key, required this.title, required this.location});
+  const TitleRow({super.key, required this.title, required this.location});
   final String title;
   final String location;
 
@@ -11,56 +11,63 @@ class TitleRow extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(32.0),
-      child: (
-        Row(mainAxisAlignment: MainAxisAlignment.center,
-          children: [Expanded(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
             child: Column(
-               crossAxisAlignment: CrossAxisAlignment.start,
-              children: [Padding(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: Text(title,style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-              ), 
-                     Text(location, style :TextStyle(fontWeight: FontWeight.w100,  color: Colors.grey[500])),
-                  ],),
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    title,
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(
+                  location,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w100,
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
           ),
-      FavoriteWidget(),
-      
-      ],)
+          FavoriteWidget(),
+        ],
       ),
     );
   }
-
 }
 
+class FavoriteWidget extends StatefulWidget {
+  const FavoriteWidget({super.key});
 
-class  FavoriteWidget extends StatefulWidget {
-
-  const FavoriteWidget ({super.key});
-
-   @override
+  @override
   State<FavoriteWidget> createState() => _FavoriteWidgetState();
 }
 
 class _FavoriteWidgetState extends State<FavoriteWidget> {
-   bool _isFavorited = true;
+  bool _isFavorited = true;
   int _favoriteCount = 41;
 
   void _toggleFavorite() {
-  setState(() {
-    if (_isFavorited) {
-      _favoriteCount -= 1;
-      _isFavorited = false;
-    } else {
-      _favoriteCount += 1;
-      _isFavorited = true;
-    }
-  });
-}
-  
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-     return Row(
+    return Row(
       mainAxisSize: MainAxisSize.min,
       children: [
         Container(
@@ -68,141 +75,123 @@ class _FavoriteWidgetState extends State<FavoriteWidget> {
           child: IconButton(
             padding: const EdgeInsets.all(0),
             alignment: Alignment.center,
-            icon: (_isFavorited
-                ? const Icon(Icons.star)
-                : const Icon(Icons.star_border)),
+            icon: (_isFavorited ? const Icon(Icons.star) : const Icon(Icons.star_border)),
             color: Colors.red[500],
             onPressed: _toggleFavorite,
           ),
         ),
         SizedBox(
           width: 18,
-          child: SizedBox(
-            child: Text('$_favoriteCount'),
-          ),
+          child: Text('$_favoriteCount'),
         ),
       ],
     );
   }
 }
 
-
-
-
-
-
-
-
-
-
 class IconCombined extends StatelessWidget {
-
-const IconCombined ({ super.key,
-required this.title, required this.icontitle
-}
-);
-final String title;
-final Icon icontitle;
+  const IconCombined({super.key, required this.title, required this.icontitle});
+  final String title;
+  final Icon icontitle;
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return(Column(children: [
-      Padding(
-        padding: const EdgeInsets.only(bottom: 4.0),
-        child: icontitle,
-      ), Text(title, style: TextStyle(color: Colors.purple ),),
-
-    ],)
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 4.0),
+          child: icontitle,
+        ),
+        Text(
+          title,
+          style: TextStyle(color: Colors.purple),
+        ),
+      ],
     );
   }
-
 }
 
 class RowButtons extends StatelessWidget {
+  const RowButtons({super.key});
 
-  const RowButtons ({
-    super.key
-  });
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Padding(
-      padding: const EdgeInsets.only(right:20,left: 20,bottom: 15),
-      child:  Row( 
+      padding: const EdgeInsets.only(right: 20, left: 20, bottom: 15),
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-               IconCombined(title:'CALL', icontitle: Icon(Icons.call,color: Colors.purple,)),
-              IconCombined(title:'ROUTE', icontitle: Icon(Icons.near_me,color: Colors.purple,)),
-              IconCombined(title:'SHARE', icontitle: Icon(Icons.share,color: Colors.purple,))
-      ]       
-      
+          IconCombined(title: 'CALL', icontitle: Icon(Icons.call, color: Colors.purple)),
+          IconCombined(title: 'ROUTE', icontitle: Icon(Icons.near_me, color: Colors.purple)),
+          IconCombined(title: 'SHARE', icontitle: Icon(Icons.share, color: Colors.purple)),
+        ],
       ),
     );
-   
   }
-
-
 }
 
-
 class FinalProduct extends StatelessWidget {
-
-  const FinalProduct ({super.key});
-
+  const FinalProduct({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return ( Column(children: [
-       Image.asset('../images/pic2001.jpg'),
-            TitleRow(title: 'Villiers-sur-marne lake from here',location: 'Just next to my house in Villiers'),
-           RowButtons(),
-            SizedBox(height: 15,),
-           Container(
-            padding: EdgeInsets.only(left: 20, right: 20),
-            child: Text('Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures  and pine forest, leads you to the lake, which warms to 20  degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.'))
-    ,SizedBox(height: 10,),
-    TextFields(),
-    
-    
-    ],)
-      
-      
-      
-      );
-  }}
+    return Column(
+      children: [
+        Image.asset('../images/pic2001.jpg'),
+        TitleRow(title: 'Villiers-sur-marne lake from here', location: 'Just next to my house in Villiers'),
+        RowButtons(),
+        SizedBox(height: 15),
+        Container(
+          padding: EdgeInsets.only(left: 20, right: 20),
+          child: Text(
+            'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.'
+          ),
+        ),
+        SizedBox(height: 10),
+        MyCustomForm(),
 
- 
+      ],
+    );
+  }
+}
 
-class TextFields extends StatelessWidget {
-const TextFields ({super.key});
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({super.key});
+  State<MyCustomForm> createState() => _MyCustomFormState();
+}
+
+class _MyCustomFormState extends State<MyCustomForm> {
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-   return (Row( 
-    mainAxisAlignment: MainAxisAlignment.spaceAround,
-    crossAxisAlignment: CrossAxisAlignment.center,
-children: [
-    Padding(
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Padding(
           padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
           child: SizedBox(
             width: 200,
             child: TextField(
-            
               decoration: InputDecoration(
-                
                 border: OutlineInputBorder(),
                 hintText: 'Enter a search term',
               ),
             ),
           ),
         ),
-         Padding(
+        Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
           child: SizedBox(
             width: 200,
             child: TextFormField(
+              controller: myController,
               decoration: const InputDecoration(
                 border: UnderlineInputBorder(),
                 labelText: 'Enter your username',
@@ -210,19 +199,45 @@ children: [
             ),
           ),
         ),
-],
-  ));
+      ],
+    );
   }
 }
+
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      appBar: AppBar(title: Center(child: Text("First Flutter Layout")),),
-      body:  SingleChildScrollView(child: Center(
-        child: FinalProduct(),
-      )),
-       
+      appBar: AppBar(
+        title: Center(child: Text("First Flutter Layout")),
+      ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: FinalProduct(),
+        ),
+      ),
+      floatingActionButton: Builder(
+        builder: (BuildContext context) {
+          return FloatingActionButton(
+            // When the user presses the button, show an alert dialog containing
+            // the text that the user has entered into the text field.
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    // Retrieve the text that the user has entered by using the
+                    // TextEditingController.
+                    content: Text(myController.text),
+                  );
+                },
+              );
+            },
+            tooltip: 'Show me the value!',
+            child: const Icon(Icons.text_fields),
+          );
+        },
+      ),
     ),
-  )
-  );
+  ));
 }
+
